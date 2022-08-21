7c7be40bff7e3ffa479e9e315474b0823f673408
Rick Elrod <rick@elrod.me>
2022-08-21 05:07:02 -0500

    link to each directory index.html, for places (cough, backblaze) that don't render index.html automatically
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- generate-html-indexes.py
+++ generate-html-indexes.py
@@ -4,6 +4,7 @@ import datetime
 import fileinput
 from pathlib import Path
 import sys
+import urllib
 
 OUTDIR = Path(sys.argv[1])
 OUTDIR.mkdir(parents=True, exist_ok=True)
@@ -27,7 +28,7 @@ li {{ font-family: monospace; }}
 </head>
 <body>
 <div id="index">
-  <h1>{path}</h1>
+  <h1>Index of: {path}</h1>
   <ul>{files}</ul>
 </div>
 <div id="footer">Generated {timestamp} by generate-html-indexes.py</div>
@@ -35,35 +36,50 @@ li {{ font-family: monospace; }}
 </html>
 """
 
+
+class IndexedFile:
+    def __init__(self, name, is_dir=False):
+        self.name = name
+        self.is_dir = is_dir
+
+    @property
+    def link(self):
+        name = urllib.parse.quote_plus(self.name)
+        if self.is_dir:
+            return name + "/index.html"
+        return name
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __str__(self):
+        return self.name
+
+
 subdirs_to_files = {}
 
 for line in sys.stdin.readlines():
-    path = Path(line)
+    path = Path(line.strip())
     parent = path.parent
 
     prev = path
     while parent != Path("."):
         if parent in subdirs_to_files:
-            if prev.name not in subdirs_to_files[parent]:
-                subdirs_to_files[parent].append(prev.name)
+            if IndexedFile(prev.name) not in subdirs_to_files[parent]:
+                print("a " + prev.name)
+                subdirs_to_files[parent].append(IndexedFile(prev.name, prev != path))
         else:
-            subdirs_to_files[parent] = [prev.name]
+            print("b " + prev.name)
+            subdirs_to_files[parent] = [IndexedFile(prev.name, prev != path)]
         prev = parent
         parent = parent.parent
-
-    if path.parent in subdirs_to_files:
-        if path.name not in subdirs_to_files[path.parent]:
-            subdirs_to_files[path.parent].append(path.name)
-    else:
-        subdirs_to_files[path.parent] = [path.name]
-
 
 for path, files in subdirs_to_files.items():
     diskpath = OUTDIR / path
     diskpath.mkdir(parents=True, exist_ok=True)
     lis = []
     for filename in files:
-        lis.append(f'<li><a href="{filename}">{filename}</a></li>')
+        lis.append(f'<li><a href="{filename.link}">{filename}</a></li>')
     with open(diskpath / "index.html", "w") as f:
         f.write(
             TEMPLATE.format(
